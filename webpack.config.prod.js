fb33284bc9a18d7192e47afe24db034f25817d2b
Consuelo Sanna <consuelo@MacBook-Air-di-Consuelo.local>
2020-03-02 12:13:54 +0100

    completed webpack base-course

---
+++ webpack.config.prod.js
@@ -0,0 +1,61 @@
+//deve dare una configurazione per webpack
+//webpack run on NodeJS
+
+const path = require("path");
+const autoprefixer = require("autoprefixer");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: ""
+  },
+  devtool: "none",
+  module: {
+    rules: [
+      {
+        /** defining that babel should handle javascript files */
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: "style-loader" },
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: {
+                localIdentName: "[name]__[local]__[hash:base64:5]"
+              }
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              ident: "postcss",
+              plugins: () => [autoprefixer()]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        loader: "url-loader?limit=8000&name=images/[name].[ext]"
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + "/src/index.html",
+      filename: "index.html",
+      inject: "body"
+    })
+  ]
+};
